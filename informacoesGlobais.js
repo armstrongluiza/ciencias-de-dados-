2ffacf436b43e21794f7e3caa8af0427e5f9d72e
armstrongluiza <114159546+armstrongluiza@users.noreply.github.com>
2024-09-30 11:39:16 -0300

    Update informacoesGlobais.js

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,13 +1,11 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function visualizarInformacoesGlobais() {
+async function vizualizarInformacoesGlobais() {
+  const res = await fetch(url)
+  const dados = await res.json()
+  cosole.log(dados);
+  const paragrafo = document.createElement('p')
+  paragrafo.classList.add('graficos-container__texto')
+}
 
-        const res = await fetch(url)
-
-        const dados = await res.json()
-
-        console.log(dados);
-
-    }
-
-    visualizarInformacoesGlobais()
+vizualizarInformacoesGlobais()
